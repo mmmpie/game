909e0c06a914bdbcd367bbc226595fcf54748837
Jonathan Hart <jon@mmmpie.com>
2015-09-21 13:03:17 -0700

    add procfile, heroku port env

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic('ex-1')).listen(80);
+connect().use(serveStatic('ex-1')).listen(process.env.PORT || 8080);
