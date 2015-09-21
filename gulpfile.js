26630120b7f759122f22af978b98e233c36b0b3a
Jonathan Hart <jon@mmmpie.com>
2015-09-21 12:09:49 -0700

    added a dummy test

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+var gulp = require('gulp');
+
+// Rerun the task when a file changes
+gulp.task('test', function() {});
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['test']);
