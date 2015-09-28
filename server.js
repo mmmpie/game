a65f82839a54cdcd495812ca5da6d6584b6844fe
Jonathan Hart <jon@mmmpie.com>
2015-09-28 07:36:05 -0700

    use express to serve files

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic('./ex-1')).listen(process.env.PORT || 8080);
+var express = require('express');
+var port = process.env.PORT || 8080;
+var app = express();
+
+app.get('/', function(request, response) {
+  response.sendfile(__dirname + '/ex-1/ex-1.html');
+});
+
+express.static(__dirname + '/ex-1/');
+
+app.listen(port);
