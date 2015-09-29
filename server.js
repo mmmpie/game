7c4810145748707a534c947ede7232024adbdb03
Jonathan Hart <jon@mmmpie.com>
2015-09-29 05:25:01 -0700

    working static server
    Got express working as a static server for the landing page and the game hour pages.

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ var port = process.env.PORT || 8080;
 var app = express();
 
 app.get('/', function(request, response) {
-  response.sendfile(__dirname + '/ex-1/ex-1.html');
+  response.sendFile(__dirname + '/static/index.html');
 });
 
-express.static(__dirname + '/ex-1/');
+app.use('/static', express.static(__dirname + '/static'));
+app.use('/hour-1', express.static(__dirname + '/hour-1'));
+app.use('/hour-2', express.static(__dirname + '/hour-2'));
 
 app.listen(port);
